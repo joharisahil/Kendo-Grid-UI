7e9a2d579ee963a29c9014aa1eca5164d63c1ca0
JohariSahil <sahiljohari04@gmail.com>
2024-05-23 17:44:44 +0530

    added some code in grid sheet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "@progress/kendo-theme-material/dist/all.css";
 import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
 import { bikeStations } from "./data/bike-stations";
 
+const BooleanCell = (props) => {
+  return <td>{props.dataItem[props.field] ? "✔️" : "❌"}</td>;
+};
+
 function App() {
   return (
     <Grid
@@ -13,8 +17,12 @@ function App() {
       <Column field="station_id" title="ID" />
       <Column field="num_bikes_available" title="Bikes Available" />
       <Column field="num_bikes_disabled" title="Bikes Disabled" />
-      <Column field="num_docs_available" title="Docks Available" />
-      <Column field="is_charging_station" title="Charging Station" />
+      <Column field="num_docks_available" title="Docks Available" />
+      <Column
+        field="is_charging_station"
+        title="Charging Station"
+        cell={BooleanCell}
+      />
       <Column field="zone" title="Zone" />
     </Grid>
   );
