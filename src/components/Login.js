82d73ee002a1ff6e02b4fbe4e1320f6740bec085
JohariSahil <sahiljohari04@gmail.com>
2024-06-02 22:42:00 +0530

    added some minor changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,15 @@
+import Logo from "../assests/Logo.png";
+
 const Login = () => {
-  return <div className="d-flex justify-content-around"></div>;
+  return (
+    <div className="d-flex justify-content-around align-items-center vh-100 bg-dark text-white">
+      <img src={Logo} alt="Logo" />
+      <div className="d-flex flex-column justify-content-around align-items-center ">
+        <div className="mb-50 display-1 fw-bold">Welcome!!</div>
+        <div>Richest</div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
