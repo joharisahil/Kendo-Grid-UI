2baeb9d49829af8f7ef416521eefe16ea7804bdc
JohariSahil <sahiljohari04@gmail.com>
2024-06-04 13:40:51 +0530

    new change

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ const Login = () => {
     <div className="d-flex justify-content-around align-items-center vh-100 bg-dark text-white">
       <img src={Logo} alt="Logo" />
       <div className="d-flex flex-column justify-content-around align-items-center ">
-        <div className="mb-50 display-1 fw-bold">Welcome!!!</div>
+        <div className="mb-50 display-1 fw-bold">Welcome!!</div>
         <div>Richest</div>
       </div>
     </div>
