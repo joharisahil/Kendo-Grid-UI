782703cb0487199e0f759209a49a454303bffbc1
JohariSahil <sahiljohari04@gmail.com>
2024-06-08 16:17:19 +0530

    added login screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "@progress/kendo-theme-material/dist/all.css";
 import MiniDrawer from "./components/Drawer";
-import Login from "./components/Login";
+import Login from "./components/login/Login";
 
 function App() {
   return <Login />;
