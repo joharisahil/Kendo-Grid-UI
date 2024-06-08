9c373d728641767a2468c1824ec9cfd27f978b9d
JohariSahil <sahiljohari04@gmail.com>
2024-06-08 19:12:32 +0530

    added navbar in erp

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,85 +1,95 @@
 import React, { useEffect, useState } from "react";
 import "./Login.css";
 // import Image from "../assets/image.png";
-import ERP_New from "../../assets/ERP_New.png";
-import Logo from "../../assets/Logo.png";
+// import ERP_New from "../../assets/ERP_New.png";
+// import Logo from "../../assets/Logo.png";
 // import GoogleSvg from "../assets/icons8-google.svg";
 
 // import { GoEyeClosed, GoEye } from "react-icons/go";
-import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+// import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  // const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="login-main">
-      <div className="login-left">
-        <img src={Logo} alt="Logo" />
-      </div>
-      <div className="login-right">
-        <div className="login-right-container">
-          <div className="login-logo">
-            <img src={ERP_New} alt="ERP_New" />
-          </div>
-          <div className="login-center">
-            <h2>Welcome back!</h2>
-            <p>Please enter your details</p>
-            <form>
-              <input
-                type="email"
-                placeholder="Email"
-                style={{ fontSize: "20px" }}
-              />
-              <div className="pass-input-div">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Password"
-                  style={{ fontSize: "20px" }}
-                />
-                {showPassword ? (
-                  <FaRegEyeSlash
-                    onClick={() => {
-                      setShowPassword(!showPassword);
-                    }}
-                  />
-                ) : (
-                  <FaRegEye
-                    onClick={() => {
-                      setShowPassword(!showPassword);
-                    }}
-                  />
-                )}
-              </div>
+    // <div className="login-main">
+    //   <div className="login-left">
+    //     <img src={Logo} alt="Logo" />
+    //   </div>
+    //   <div className="login-right">
+    //     <div className="login-right-container">
+    //       <div className="login-logo">
+    //         <img src={ERP_New} alt="ERP_New" />
+    //       </div>
+    //       <div className="login-center">
+    //         <h2>Welcome back!</h2>
+    //         <p>Please enter your details</p>
+    //         <form>
+    //           <input
+    //             type="email"
+    //             placeholder="Email"
+    //             style={{ fontSize: "20px" }}
+    //           />
+    //           <div className="pass-input-div">
+    //             <input
+    //               type={showPassword ? "text" : "password"}
+    //               placeholder="Password"
+    //               style={{ fontSize: "20px" }}
+    //             />
+    //             {showPassword ? (
+    //               <FaRegEyeSlash
+    //                 onClick={() => {
+    //                   setShowPassword(!showPassword);
+    //                 }}
+    //               />
+    //             ) : (
+    //               <FaRegEye
+    //                 onClick={() => {
+    //                   setShowPassword(!showPassword);
+    //                 }}
+    //               />
+    //             )}
+    //           </div>
 
-              <div className="login-center-options">
-                <div className="remember-div">
-                  <input type="checkbox" id="remember-checkbox" />
-                  <label htmlFor="remember-checkbox">Remember me</label>
-                </div>
-                <a href="#" className="forgot-pass-link">
-                  Forgot password?
-                </a>
-              </div>
-              <div className="login-center-buttons">
-                <button type="button">Log In</button>
-                {/* <button type="button">
-                  <img src="https://via.placeholder.com/150" alt="" />
-                  Log In with Google
-                </button> */}
-              </div>
-            </form>
-          </div>
+    //           <div className="login-center-options">
+    //             <div className="remember-div">
+    //               <input type="checkbox" id="remember-checkbox" />
+    //               <label htmlFor="remember-checkbox">Remember me</label>
+    //             </div>
+    //             <a href="#" className="forgot-pass-link">
+    //               Forgot password?
+    //             </a>
+    //           </div>
+    //           <div className="login-center-buttons">
+    //             <button type="button">Log In</button>
+    //             {/* <button type="button">
+    //               <img src="https://via.placeholder.com/150" alt="" />
+    //               Log In with Google
+    //             </button> */}
+    //           </div>
+    //         </form>
+    //       </div>
 
-          {/* <p className="login-bottom-p">
-            Don't have an account? <a href="#">Sign Up</a>
-          </p> */}
-          <p className="login-bottom-p">
-            Powered by
-            <p>techeefy.com</p>
-          </p>
-        </div>
+    //       {/* <p className="login-bottom-p">
+    //         Don't have an account? <a href="#">Sign Up</a>
+    //       </p> */}
+    //       <p className="login-bottom-p">
+    //         Powered by
+    //         <p>techeefy.com</p>
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
+    <>
+      <div className="body">
+        <nav className="navbar bg-body">
+          <div className="container-fluid">
+            <h1 className="navbar-text-style navbar-text fw-bold ">techeefy</h1>
+          </div>
+        </nav>
+        sahil{" "}
       </div>
-    </div>
+    </>
   );
 };
 
